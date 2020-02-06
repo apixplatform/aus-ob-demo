310832882f57c7529b26d232f919aaddca2a77f2
Pubudu Welagedara <pubuduwelagedara@gmail.com>
2020-02-06 14:55:33 +0530

    Update config.js

--- utils/config/config.js
+++ utils/config/config.js
@@ -2,13 +2,13 @@
 
 module.exports = function(app) {
 
-	app.set('ALGORITHM', process.env.EXPIRATION || 'PS256');
-	app.set('AUTH_METHOD', process.env.EXPIRATION || 'private_key_jwt');
+	app.set('ALGORITHM', process.env.ALGORITHM || 'PS256');
+	app.set('AUTH_METHOD', process.env.AUTH_METHOD || 'private_key_jwt');
 	app.set('EXPIRATION', process.env.EXPIRATION || '3600');
-	app.set('ASSERTION_ALGORITHM', process.env.EXPIRATION || 'RS256');
-	app.set('WSO2_HOST', process.env.EXPIRATION || 'cua.wso2support.com');
-	app.set('WSO2_PORT', process.env.EXPIRATION || '8243');
+	app.set('ASSERTION_ALGORITHM', process.env.ASSERTION_ALGORITHM || 'RS256');
+	app.set('WSO2_HOST', process.env.WSO2_HOST || 'cua.wso2support.com');
+	app.set('WSO2_PORT', process.env.WSO2_PORT || '8243');
 	
 	app.set('SSA_KID_VALUE', process.env.SSA_KID_VALUE || 'b8facf2ff39444f781e0be5db4b14f16');
-	app.set('DCR_KID_VALUE', process.env.SSA_KID_VALUE || 'xII5m7uKSgSBsDlhARywqVco7IE');
+	app.set('DCR_KID_VALUE', process.env.DCR_KID_VALUE || 'xII5m7uKSgSBsDlhARywqVco7IE');
 };
